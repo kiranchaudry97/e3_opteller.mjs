051ab25c283c4342014425c0f9f585db800f930b
kiranchaudry97 <kiran.chaudry@outlook.be>
2024-09-24 16:21:53 +0200

    Update oefening2.mjs
    - oefening M1 oefening 2 jouw leeftijd op toekomstige jaar
    - oefening  optellen naar aantal seconden

--- oefening2.mjs
+++ oefening2.mjs
@@ -2,7 +2,7 @@ import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 
-
+//M1 oefening 2 jouw leeftijd op toekomstige jaar //
 
 let jeGeboorteJaar = await userInput.question('Wat is je geboortejaar? ');
 console.log(jeGeboorteJaar);
@@ -15,6 +15,24 @@ console.log(som);
 
 
 
-console.log('Hallo ' + jeGeboorteJaar + 'mijn geboortejaar ' + jeToekomstJaar +' In 2030 word ik' + som +  'jaar oud');
+console.log('Hallo ' + jeGeboorteJaar + 'mijn geboortejaar ' + jeToekomstJaar +' In 2030 word ik' + som +   'jaar oud');
+
+// oefening optellen naar aantal seconden//
+
+let dagen = await userInput.question('Geef het aantal dagen? ');
+console.log(dagen);
+
+let uren = await userInput.question('Geef het aantal uren? ');
+console.log(uren);
+
+let minuten = await userInput.question('Geef het aantal minuten? ');
+console.log(minuten);
+
+let seconden = await userInput.question('Geef het aantal seconden? ');
+console.log(seconden);
+
+let AantalSeconden = (dagen * 24 * 60 * 60 ) + (uren * 60 * 60 ) + (minuten * 60 ) + seconden;
+
+console.log('Hallo' + dagen + ' Geef het aantal dagen ' + uren +  ' Geef het aantal uren ' + minuten + 'Geef het aantal minuten ' + seconden + ' Geef het aantal seconden ' + AantalSeconden + 'Aantal seconden');
 
 process.exit();
